51b58d186d8a1e5ebe513f9c00d7f85ff2a53a2d
judegordon <jude2gordon@gmail.com>
2026-05-21 17:51:42 +0100

    Initial Commit

---
+++ shutokuTests/shutokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  shutokuTests.swift
+//  shutokuTests
+//
+//  Created by Jude on 21/05/2026.
+//
+
+import Testing
+@testable import shutoku
+
+struct shutokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
